711cbdfa3fcfc8bda55952a1751ae5e67c06cd72
御风 <54386576+hongyingwei@users.noreply.github.com>
2024-04-18 19:51:21 +0800

    springmvc-数据校验

---
+++ springmvc-demo/src/main/java/com/javaee/springmvc/pojo/Student.java
@@ -0,0 +1,40 @@
+package com.javaee.springmvc.pojo;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+public class Student {
+	
+    @NotBlank(message = "用户名不能为空")
+    @Size(min = 3, max = 20, message = "用户名长度必须在3到20个字符之间")
+    private String username;
+ 
+    @NotBlank(message = "密码不能为空")
+    @Size(min = 6, max = 20, message = "密码长度必须在6到20个字符之间")
+    private String password;
+
+    // getter和setter方法
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	@Override
+	public String toString() {
+		return "Student [username=" + username + ", password=" + password + "]";
+	}
+	
+	
+ 
+}
