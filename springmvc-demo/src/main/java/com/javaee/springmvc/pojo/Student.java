739cb74720fa7cd4d44151f7d9db7acf908571b8
御风 <54386576+hongyingwei@users.noreply.github.com>
2024-04-19 17:48:41 +0800

    修改

--- springmvc-demo/src/main/java/com/javaee/springmvc/pojo/Student.java
+++ springmvc-demo/src/main/java/com/javaee/springmvc/pojo/Student.java
@@ -3,6 +3,7 @@ package com.javaee.springmvc.pojo;
 import java.io.Serializable;
 
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
 
@@ -16,7 +17,7 @@ public class Student implements Serializable{
     @Size(min = 6, max = 20, message = "密码长度必须在6到20个字符之间")
     private String password;
     
-    @Pattern(regexp = "^1(3|4|5|7|8)\\d{9}$",message = "手机号码格式错误")
+    @Pattern(regexp = "^1(3|4|5|7|8)\\d{9}$", message = "手机号码格式错误")
     public String phone;
 
     // getter和setter方法
